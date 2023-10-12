a42cf5c2f2aef29139aae4327c79d10653d1f16f
Adel Golghalyani <ad96el@gmail.com>
2023-10-12 10:20:44 +0200

    backend

--- frontend/src/layout/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Layout, LayoutProps } from "react-admin";
-import AppBar from "./AppBar";
-import Menu from "./Menu";
-
-// eslint-disable-next-line react/display-name
-export default (props: LayoutProps) => (
-  <Layout {...props} appBar={AppBar} menu={Menu} />
-);
